755a4607641a7d2dbe3c2df39e7a90aa06fdb4e7
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-03-21 20:43:52 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,11 @@
-function getResponseFromAPI(url) {
+const getResponseFromAPI = () => {
   return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        resolve(xhr.responseText);
-      } else {
-        reject(`Error fetching data from API. Status code: ${xhr.status}`);
-      }
-    };
-    xhr.onerror = () => {
-      reject("Error fetching data from API. Please try again later.");
-    };
-    xhr.send();
+    // Make an API call
+    const response = callAPI();
+    if (response) {
+      resolve(response);
+    } else {
+      reject(Error('API call failed'));
+    }
   });
-}
+};
