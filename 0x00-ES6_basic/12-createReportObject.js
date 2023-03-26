ba22205fef39c14cde0c631a7a8352bb19fdee7d
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-03-26 21:04:35 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,17 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-  for (let i = 0; i < employeesList.length; i++) {
-    const employee = employeesList[i];
-    if (!allEmployees[employee.department]) {
-      allEmployees[employee.department] = [];
-    }
-    allEmployees[employee.department].push(employee.name);
-  }
-
   return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(allEmployees).length;
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
 }
